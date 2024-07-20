a38586694217443f8ccd16e46ff25688c5efc716
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-19 17:51:38 -0700

    iniate the files

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+
+
 
 export default function App() {
   return (
---
+++ screens/AddActivity.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddActivity = () => {
+  return (
+    <View>
+      <Text>AddActivity</Text>
+    </View>
+  )
+}
+
+export default AddActivity
